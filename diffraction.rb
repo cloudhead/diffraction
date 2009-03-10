356d70a4bc66c0533e9ef742ff40ac21cfbbb332
cloudhead <biopunk@gmail.com>
2009-03-10 16:43:23 -0400

    more comments

--- diffraction.rb
+++ diffraction.rb
@@ -4,6 +4,7 @@ require 'json'
 require 'cgi'
 
 class Diffraction
+  MAX = 256
   def initialize *args
     if args.count > 0      
       @quote = args.first
@@ -17,11 +18,11 @@ class Diffraction
     request  = Rack::Request.new env
     response = Rack::Response.new
     
-    # Break path into discreet parts & trim slashes
+    # Break path into quote/year & discard pre and post slashes
     quote, year = CGI.unescape( request.path_info ).
-                  gsub( /^\/?([\w\-^\/]+)\/?$/, "\\1").
+                  gsub(/^\/?([\w\-^\/]+)\/?$/, "\\1").
                   split('/')
-    puts "quote:" + quote + " year:" + year
+    # Create a new Diffraction object, fetch the quotes, and convert to json
     response.body = self.class.new( quote, year ).fetch(:close).to_json
     response['Content-Type'] = 'json'
     response['Content-Length'] = response.body.size.to_s
@@ -29,7 +30,8 @@ class Diffraction
   end
   
   def fetch key = nil
-    open( URL.new( @quote, @year ).to_str ) do |table|
+    # Open a connection to the feed
+    open( URL.new( @quote, @year ).to_s ) do |table|
       table.each do |s|
         row = {}
         row[:date], 
@@ -47,10 +49,13 @@ class Diffraction
       @data.shift                                   # Discard first row
     end
     
+    # If a key was provided, only return that column.
+    # Reverse the array, to get the rows in chronological order.
+    # Normalize the rows.
     if key
-      normalize @data.collect { |i| i[ key ] }.reverse, 256
+      normalize @data.collect { |i| i[ key ] }.reverse, MAX
     else
-      normalize @data.reverse, 256
+      normalize @data.reverse, MAX
     end
   end
   
@@ -70,15 +75,9 @@ class Diffraction
   end
   
   class URL    
-    BASE = "http://ichart.finance.yahoo.com/table.csv?"
-    GAP = { day: 'd', week: 'w', month: 'm' }
-    PARAMS = { 
-      quote: 's', 
-      gap: 'g', 
-      month: 'a', 
-      day: 'b', 
-      year: 'c'
-    }
+    BASE = "http://ichart.finance.yahoo.com/table.csv?" # The base url
+    GAP = { day: 'd', week: 'w', month: 'm' } # The frequency of the quotes (daily, monthly...)
+    PARAMS = { quote: 's', gap: 'g', month: 'a', day: 'b', year: 'c' } # URL parameter names
     
     def initialize quote, year = 1986, gap = :day
       @url = { 
@@ -91,7 +90,8 @@ class Diffraction
       self
     end
     
-    def to_str
+    # Construct the URL
+    def to_s
       BASE + @url.collect do |k, v|
         PARAMS[ k ] + '=' + CGI.escape( v.to_s ) + '&'
       end.join
--- js/diffraction.js
+++ js/diffraction.js
@@ -1,19 +1,21 @@
 $( document ).ready( function() {
-	$("#refresh").click( function() {
-	    /* Send POST request to server, with textarea contents */
-    	$.get( $("#index").val() + '/' + $("#year").val(), function( json ) {
-    	    var quotes = eval( json );
-    	    var spectrum = '';
-    	    
-    	    for( var i in quotes ) {
-    	        spectrum += "<div class='bar' style='background-color: hsl(" + 
-    	                    ( 256 - Math.round( quotes[ i ] ) ) + 
-    	                    ",50%," + "50" + "%);'></div>";
-    	    }
-    		$("div#spectrum").fadeOut('slow', function() { 
-    		    $( this ).html( spectrum ).fadeIn('medium'); 
-    		});	
-    	});
-    	return false;
- 	});
+    $("#refresh").click( function() {
+        /* Fetch quotes from server */
+        $.get( $("#index").val() + '/' + $("#year").val(), function( json ) {
+            var quotes = eval( json ); /* Convert json data into object */
+            var spectrum = '';
+            
+            /* Create the spectrum */
+            for( var i in quotes ) {
+                spectrum += "<div class='bar' style='background-color: hsl(" + 
+                            ( 256 - Math.round( quotes[ i ] ) ) + 
+                            ",50%," + "50" + "%);'></div>";
+            }
+            /* Hide the old spectrum and show the new one */
+            $("div#spectrum").fadeOut('slow', function() { 
+                $( this ).html( spectrum ).fadeIn('medium'); 
+            }); 
+        });
+        return false;
+    });
 });
